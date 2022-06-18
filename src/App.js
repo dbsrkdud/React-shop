c711cdab32c1d253b7c04c17d64b02991d1bfcd7
dbsrkdud <yungayoung63@gmail.com>
2022-06-18 22:18:02 +0900

    컴포넌트, Map 사용하기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Navbar, Container, Nav } from 'react-bootstrap';
+import 메인사진 from './img/bg.png';
+import { useState } from 'react';
+import data from './data.js';
 
 function App() {
+  let [shoes] = useState(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Shop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Category</Nav.Link>
+            <Nav.Link href="#pricing">Board</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="main-bg" style={ { backgroundImage : 'url(' + 메인사진 + ')' } }></div>
+      <Product shoes={ shoes } />
+
+      {/* <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
+            PUBLIC 폴더 내 img 사용시 권장방법
+            <img src={ process.env.PUBLIC_URL + "/logo192.png" } width="80%" />
+            <h4>{ shoes[0].title }</h4>
+            <h4>{ shoes[0].price }</h4>
+          </div>
+          <div className="col-md-4">
+          <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
+            <h4>{ shoes[1].title }</h4>
+            <h4>{ shoes[1].price }</h4>
+          </div>
+          <div className="col-md-4">
+          <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
+            <h4>{ shoes[2].title }</h4>
+            <h4>{ shoes[2].price }</h4>
+          </div>
+        </div>        
+      </div> */}
+
     </div>
   );
+}
+
+function Product(props){
+  let data = [...props.shoes];
+  return (
+    <div className="container">
+        <div className="row">
+        {
+          data.map((a, i) => {
+            return (
+              <div className="col-md-4">
+                <img src= { `https://codingapple1.github.io/shop/shoes${ (i + 1) }.jpg` } width="80%" />
+                <h4>{ a.title }</h4>
+                <h4>{ a.price }</h4>
+              </div>
+            )
+          })
+        }
+        </div>
+    </div>
+  )
+
 }
 
 export default App;
