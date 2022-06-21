28e86761cb583990ebf63d46ca33b14e189eab99
dbsrkdud <yungayoung63@gmail.com>
2022-06-21 22:13:07 +0900

    useNavigate, Outlet

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import { Button, Navbar, Container, Nav } from 'react-bootstrap';
 import 메인사진 from './img/bg.png';
 import { useState } from 'react';
 import data from './data.js';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
+import Detail from './routes/Detail.js';
 
 function App() {
   let [shoes] = useState(data);
+  let navigate = useNavigate();
+
   return (
     <div className="App">
 
@@ -14,51 +17,55 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">Shop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Category</Nav.Link>
+            <Nav.Link onClick={ () => { navigate('/') } }>Home</Nav.Link>
+            <Nav.Link onClick={ () => { navigate('/detail') } }>Product</Nav.Link>
             <Nav.Link href="#pricing">Board</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
-      <Link to="/">홈 </Link>
-      <Link to="/detail">상세페이지</Link>
-
       <div className="main-bg" style={ { backgroundImage : 'url(' + 메인사진 + ')' } }></div>
-
-
+      
       <Routes>
         <Route path="/" element={ 
           <Product shoes={ shoes } />
         }>
         </Route>
-        <Route path="/detail" element={ <div>상세페이지</div> } />
-      </Routes>
+        <Route path="/detail" element={ <Detail /> } />
 
-      {/* <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
-            PUBLIC 폴더 내 img 사용시 권장방법
-            <img src={ process.env.PUBLIC_URL + "/logo192.png" } width="80%" />
-            <h4>{ shoes[0].title }</h4>
-            <h4>{ shoes[0].price }</h4>
-          </div>
-          <div className="col-md-4">
-          <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
-            <h4>{ shoes[1].title }</h4>
-            <h4>{ shoes[1].price }</h4>
-          </div>
-          <div className="col-md-4">
-          <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
-            <h4>{ shoes[2].title }</h4>
-            <h4>{ shoes[2].price }</h4>
-          </div>
-        </div>        
-      </div> */}
+        <Route path="/about" element={ <About /> } >
+          <Route path="member" element={ <div>멤버</div> } />
+          <Route path="location" element={ <About /> } />
+        </Route>
+
+        <Route path="/event" element={ <Event /> } >
+          <Route path="one" element={ <h5>첫 주문시 양배추즙 서비스</h5> } />
+          <Route path="two" element={ <h5>생일기념 쿠폰받기</h5> } />
+        </Route>
+        
+        <Route path="*" element={ <div>404</div> } />
+      </Routes>
 
     </div>
   );
+}
+
+function About(){
+  return(
+    <div>
+      <h4>회사정보</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+function Event(){
+  return(
+    <div>
+      <h4>오늘의 이벤트</h4>
+      <Outlet></Outlet>
+    </div>
+  )
 }
 
 function Product(props){
