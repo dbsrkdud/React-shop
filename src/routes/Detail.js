01f5b69d8a5a815834e37e6ddd03d9061d44b089
dbsrkdud <yungayoung63@gmail.com>
2022-07-01 20:43:44 +0900

    animation

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -101,14 +101,21 @@ function Detail(props){
 }
 
 
-function TabContent(props) {
-    if(props.탭 == 0) {
-        return <div>내용0</div>
-    } else if( props.탭 == 1){
-        return <div>내용1</div>
-    } else if(props.탭 == 2){
-        return <div>내용2</div>
-    }
+function TabContent({탭}) {
+    
+    let [fade, setFade] = useState('');
+
+    useEffect( () => {
+        let a = setTimeout( () => { setFade('end') }, 100)
+        return () => {
+            clearTimeout(a);
+            setFade('');
+        }
+    }, [탭])
+
+    return ( <div className={'start ' + fade}>
+        { [<div>내용0</div>, <div>내용1</div>, <div>내용2</div>][탭] }
+    </div> )
 }
 
 export default Detail;
